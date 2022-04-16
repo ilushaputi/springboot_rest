3a3b9505fe41c1c91fbfbfbcbaccbdaa179ae645
ilushaputi <putintzeff.ilya@yandex.com>
2022-04-16 10:59:54 +0300

    Lesson 3 part 1 Create REST API

---
+++ src/main/java/com/ilyaputintsev/spring/springboot/springboot_rest/dao/EmployeeDAO.java
@@ -0,0 +1,17 @@
+package com.ilyaputintsev.spring.springboot.springboot_rest.dao;
+
+
+
+import com.ilyaputintsev.spring.springboot.springboot_rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee> getAllEmployees();
+
+   //public void saveEmployee(Employee employee);
+
+   //public Employee getEmployee(int id);
+
+   //public void deleteEmployee(int id);
+}
