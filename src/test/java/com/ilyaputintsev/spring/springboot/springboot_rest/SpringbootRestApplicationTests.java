16b42419b82c65302afb752d13480b36cf0cf160
ilushaputi <putintzeff.ilya@yandex.com>
2022-04-16 10:59:54 +0300

    Start learning spring boot

---
+++ src/test/java/com/ilyaputintsev/spring/springboot/springboot_rest/SpringbootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilyaputintsev.spring.springboot.springboot_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
